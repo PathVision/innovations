22bc1b144a6d0237b9ab6262ec33d20d2faf2ed9
yatheendra-reddy <yatheendranathreddydevasani@gmail.com>
2026-01-07 21:32:44 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   // Config for GitHub Pages (only applies during 'npm run build')
   output: isProd ? 'export' : undefined,
-  basePath: isProd ? '/PathVision-IMAS' : undefined,
+   basePath: '/PathVision-PathVision-IMAS', : undefined,
+  assetPrefix: '/PathVision-PathVision-IMAS/',
   reactStrictMode: true,
   images: {
     unoptimized: true,
